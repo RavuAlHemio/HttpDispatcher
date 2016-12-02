16e4683441dea260f91a8c0e11e723c3c67bc2ed
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2016-12-02 11:11:05 +0100

    HttpDispatcher: let dotnet autogenerate some assembly attribs from projfile
    Let the dotnet build system autogenerate some assembly attributes from
    project.json. This means it's enough to update the version once per project
    (in project.json) when releasing new versions.

--- RavuAlHemio.HttpDispatcher/Properties/AssemblyInfo.cs
+++ RavuAlHemio.HttpDispatcher/Properties/AssemblyInfo.cs
@@ -3,21 +3,11 @@
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("RavuAlHemio.HttpDispatcher")]
-[assembly: AssemblyDescription("HttpListener wrapper which forwards requests to handler objects' endpoint methods.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ond\u0159ej Ho\u0161ek")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright \u00A9 Ond\u0159ej Ho\u0161ek")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("0.0.22")]
-[assembly: AssemblyFileVersion("0.0.22")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
